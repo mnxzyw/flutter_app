5ec43657b7fca1c9f01d770ac8eef4b60caa5f6a
Anonymous <Anonymous>
2020-04-03 16:25:08 +0800

    添加控件测试

--- lib/main.dart
+++ lib/main.dart
@@ -113,6 +113,54 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  ///返回一个居中带图标和文本的Item
+  _getBottomItem(IconData icon, String text) {
+    ///充满 Row 横向的布局
+    return new Expanded(
+      flex: 1,
+
+      ///居中显示
+      child: new Center(
+
+        ///横向布局
+        child: new Row(
+
+          ///主轴居中,即是横向居中
+          mainAxisAlignment: MainAxisAlignment.center,
+
+          ///大小按照最大充满
+          mainAxisSize: MainAxisSize.max,
+
+          ///竖向也居中
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+
+            ///一个图标，大小16.0，灰色
+            new Icon(
+              icon,
+              size: 16.0,
+              color: Colors.grey,
+            ),
+
+            ///间隔
+            new Padding(padding: new EdgeInsets.only(left: 5.0)),
+
+            ///显示文本
+            new Text(
+              text,
+              //设置字体样式：颜色灰色，字体大小14.0
+              style: new TextStyle(color: Colors.grey, fontSize: 14.0),
+              //超过的省略为...显示
+              overflow: TextOverflow.ellipsis,
+              //最长一行
+              maxLines: 1,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -168,6 +216,74 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.pushNamed(context, "tip_route",
                     arguments: "tip_route");
               },
+            ),
+            Container(
+
+              ///四周10大小的maring
+                margin: EdgeInsets.all(10.0),
+                height: 120.0,
+                width: 500.0,
+
+                ///透明黑色遮罩
+                decoration: new BoxDecoration(
+
+                  ///弧度为4.0
+                    borderRadius: BorderRadius.all(Radius.circular(4.0)),
+
+                    ///设置了decoration的color，就不能设置Container的color。
+                    color: Colors.blue,
+
+                    ///边框x
+                    border:
+                    new Border.all(color: Color(0xFF42A5F5), width: 0.3)),
+                child: new Text("666666")),
+            Container(
+
+              ///卡片包装
+              child: new Card(
+
+                ///增加点击效果
+                  child: new FlatButton(
+                      onPressed: () {
+                        print("点击了哦");
+                      },
+                      child: new Padding(
+                        padding: new EdgeInsets.only(
+                            left: 0.0, top: 10.0, right: 10.0, bottom: 10.0),
+                        child: new Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+
+                            ///文本描述
+                            new Container(
+                                child: new Text(
+                                  "这是一点描述",
+                                  style: TextStyle(
+                                    color: Color(0xFF42A5F5),
+                                    fontSize: 14.0,
+                                  ),
+
+                                  ///最长三行，超过 ... 显示
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                margin:
+                                new EdgeInsets.only(top: 6.0, bottom: 2.0),
+                                alignment: Alignment.topLeft),
+                            new Padding(padding: EdgeInsets.all(10.0)),
+
+                            ///三个平均分配的横向图标文字
+                            new Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                _getBottomItem(Icons.star, "1000"),
+                                _getBottomItem(Icons.link, "1000"),
+                                _getBottomItem(Icons.subject, "1000"),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ))),
             ),
           ],
         ),
